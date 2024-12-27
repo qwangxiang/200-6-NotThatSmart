74f1ec687a8f32d300f7920a2183770efdca7f91
qwangxiang <qwangxiang@sjtu.edu.cn>
2024-12-27 20:06:29 +0800

    添加help

--- pages/4_数据下载.py
+++ pages/4_数据下载.py
@@ -27,9 +27,9 @@ if __name__ == '__main__':
     with col2:
         with open('DeviceInfo.txt', 'rb') as f:
             data = f.read()
-        st.download_button(label='mac文件', data=data, file_name='DeviceInfo.txt', mime='text/plain')
+        st.download_button(label='mac文件下载', data=data, file_name='DeviceInfo.txt', mime='text/plain', help='下载mac地址参考文件')
     with col3:
-        st.download_button(label='下载数据', data=df.to_csv(encoding='utf-8-sig'), file_name='data.csv', mime='text/csv')
+        st.download_button(label='下载数据', data=df.to_csv(encoding='utf-8-sig'), file_name='data.csv', mime='text/csv', help='根据上述选择下载数据')
             
     # 下载按钮居中
     
